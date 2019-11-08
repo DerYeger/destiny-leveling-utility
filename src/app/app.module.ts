95f8a1ef4b3f322cbf365e864cd6f72b1ff4735d
Jan Müller <janmueller3698@gmail.com>
2019-11-09 00:50:21 +0100

    Added pwa and firebase support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import {AppComponent} from './app.component';
 import {ModelService} from './model/model.service';
 import {FormsModule} from '@angular/forms';
 import {CharacterModule} from './character/character.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import {CharacterModule} from './character/character.module';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    CharacterModule
+    CharacterModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [ModelService],
   exports: [],
